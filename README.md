e9aa7b5152a0cc238610ca557b64ceb8145f1821
jwang0110 <johnw7@uci.edu>
2021-05-16 19:46:48 -0700

    connect server to redis

--- README.md
+++ README.md
@@ -11,3 +11,14 @@
 
 - https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
 - https://node-postgres.com/
+
+## Express Session
+
+- http://expressjs.com/en/advanced/best-practice-security.html#use-cookies-securely
+- https://github.com/expressjs/session#compatible-session-stores
+
+## Redis
+
+- https://redislabs.com/blog/redis-on-windows-10/
+- https://redis.io/topics/quickstart
+- https://github.com/tj/connect-redis
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import session from "express-session";
+import redis from "redis";
+import connectRedis from "connect-redis";
 import cors from "cors";
 
+import { __prod__, COOKIE_NAME } from "./constants";
 import User from "./controllers/user";
 
 // Constants
@@ -9,8 +13,29 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.use(express.json());
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
+
+app.set("trust porxy", 1);
 app.use(cors());
+
+app.use(
+	session({
+		store: new RedisStore({ client: redisClient }),
+		name: COOKIE_NAME,
+		cookie: {
+			maxAge: 1000 * 60 * 60 * 7, // 7 days
+			httpOnly: true,
+			sameSite: "lax",
+			secure: __prod__,
+		},
+		secret: "secret",
+		saveUninitialized: false,
+		resave: false,
+	})
+);
+
+app.use(express.json());
 
 app.get("/", (_, res) => {
 	res.send("Hello World");
