a36c7550580acfbfd86bc7c72c1ff9fff6065ef9
jwang0110 <johnw7@uci.edu>
2021-06-03 17:18:35 -0700

    fixed cookie problem

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const app = express();
 const RedisStore = connectRedis(session);
 const redis = new Redis(process.env.REDIS_URL);
 
-app.set("trust porxy", 1);
+app.set("trust proxy", 1);
 app.use(
 	cors({
 		origin: process.env.CORS_ORIGIN,
